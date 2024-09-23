cf97f4088f0ad62cf3d9b245e45567351f14479b
pabloflores465 <pablopolis2016@gmai.com>
2024-09-23 12:24:19 -0600

    adding error, router and home components to the proyect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { Link, createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './pages/Login';
+import React from 'react';
+import ErrorPage from './pages/ErrorPage';
+import Home from './pages/Home';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <Login/>,
+    errorElement:  <ErrorPage/>
+  },
+  {
+    path: '/home',
+    element: <Home/>
+  }
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
   );
 }
 
